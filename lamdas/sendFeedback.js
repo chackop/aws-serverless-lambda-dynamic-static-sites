fd0001e47e2c78c2adf1cc277df90e3ef217ce23
ChxDesk <chackop@gmail.com>
2021-01-11 19:11:28 +0000

    updated for cors

--- lamdas/sendFeedback.js
+++ lamdas/sendFeedback.js
@@ -1,13 +1,14 @@
-const querystring = require('querystring');
+const querystring = require("querystring");
 
 exports.handler = async (event) => {
-    
-    const params = querystring.parse(event.body);
-    
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Thank you, ' + params['name'] + '! ' +
-                             'Your feedback was received!'),
-    };
-    return response;
+  const params = querystring.parse(event.body);
+
+  const response = {
+    statusCode: 200,
+    headers: { "Access-Control-Allow-Origin": "http://chacsdev.com" },
+    body: JSON.stringify(
+      "Thank you, " + params["name"] + "! " + "Your feedback was received!"
+    ),
+  };
+  return response;
 };
